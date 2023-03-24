3796c7d538552a2e966e47ec4a29727e8caaba96
leeyh1000 <codingloveofc@gmail.com>
2023-03-24 17:22:49 +0900

    step1

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -1,3 +1,4 @@
+package step1;
 import java.util.Scanner;
 
 public class Main {
@@ -5,8 +6,16 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("-프로그램 시작-");		
 		Scanner sc = new Scanner(System.in);
-		String cmd = sc.nextLine();
-		System.out.println("입력 명령어 : "+cmd);
+		
+		while(true) {
+			System.out.printf("명령어 입력해 주세요: ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}				
+		}
+	
 		System.out.println("-프로그램 끝-");
 		sc.close();
 	}
